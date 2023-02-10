41fa40cd2e4a104fcca491a76908f18e23d4d61a
Rayyan-waseem <64113351+Rayyan-waseem@users.noreply.github.com>
2023-02-10 10:05:25 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,59 @@
+#This is a comment
+print("Hello, World!")
+
+#This is a Variable
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#This is a String
+a = "Hello"
+print(a)
+
+
+#This is a List
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+#This is a Tuple
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+#This is a Set
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+
+#This is a Dictionary 
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+
+#This is a If ... Else 
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+
+#This is a WhileLoop
+  i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+
+ #This is a ForLoop
+  fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
